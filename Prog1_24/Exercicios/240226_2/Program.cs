a84c2b549069104ae741024c466243dfe79f6baf
Julio Cesar Nardi <407472@unoesc.lan>
2024-02-26 20:09:38 -0300

    Primeiro Commit

---
+++ Prog1_24/Exercicios/240226_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Trabalhando com variaveis em C#
+
+// Strings 
+// Declarando variavel em inicialização
+string message1 = "1";
+
+// Inicializando variavel com nulo
+string message2 = null;
+
+// inicializando a string vazia
+
+string message3 = System.String.Empty; // "";
+
+// Declarar uma string com valor implicito
+
+var message4 = "PASTEL";
+
+Console.WriteLine(message4);
+
+// Concatenando Strings
+string concat = message1 + " " + message2 + " " + message3 + " " + message4;
+Console.WriteLine("\n " + concat);
