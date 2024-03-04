3866105ce5e15e6982f326ad3cfce8ae4d3d4b20
Julio Cesar Nardi <407472@unoesc.lan>
2024-03-04 19:10:12 -0300

    Segundo Commit

---
+++ Prog1_24/Atividades/240226_2_Formulario/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// Variaveis:
+
+int opcao;
+
+String nome;
+String email;
+String nascimento;
+String genero;
+// Endereço (⌐■_■)
+String pais;
+String estado;
+String cidade;
+String bairro;
+String rua;
+String numero;
+String cep;
+
+// Recebimento de Valores
+
+Console.WriteLine("Informe seu nome");
+nome = Console.ReadLine();
+
+Console.WriteLine("Informe seu e-mail");
+email = Console.ReadLine();
+
+Console.WriteLine("Informe sua data de nascimento");
+nascimento = Console.ReadLine();
+
+Console.WriteLine("Informe seu genero");
+genero = Console.ReadLine();
+
+Console.WriteLine("Informe seu pais de origem");
+pais = Console.ReadLine();
+
+Console.WriteLine("Informe seu estado de origem");
+estado = Console.ReadLine();
+
+Console.WriteLine("Informe sua cidade de origem");
+cidade = Console.ReadLine();
+
+Console.WriteLine("Informe sua rua");
+rua = Console.ReadLine();
+
+Console.WriteLine("Informe o numero da sua casa");
+numero = Console.ReadLine();
+
+Console.WriteLine("Informe seu cep");
+cep = Console.ReadLine();
+
+
+// Escreve o formulario (～￣▽￣)～
+
+Console.WriteLine(
+    "--------------------------------\n" +
+    "---- Formulario De Cadastro ----\n" +
+    $"---- Nome: {nome}\n" +
+    $"---- Email: {email}\n" +
+    $"---- Nascimento: {nascimento}\n" +
+    $"---- Genero: {genero}\n" +
+    "----        Endereco        ----\n" +
+    $"---- Pais: {pais}\n" +
+    $"---- Estado: {estado}\n" +
+    $"---- Cidade: {cidade}\n" +
+    $"---- Rua: {rua}\n" +
+    $"---- Numero: {numero}\n" +
+    $"---- CEP: {cep}\n" +
+    "--------------------------------");
+
+
