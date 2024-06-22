032b6ef4e5d2c8d683607392f29dd7e781b6f54d
Julio Cesar Nardi <julio.cesarnardi88@gmail.com>
2024-06-21 22:12:06 -0300

    Calculadora e Tabuada

---
+++ Prog1_24/Atividades/Calculadora/Program.cs
@@ -0,0 +1,69 @@
+﻿// Lê o primeiro valor
+Console.WriteLine("Determine o valor do primeiro numero");
+double FirstValue = Convert.ToDouble(Console.ReadLine());
+
+
+Console.WriteLine("Determine o valor do primeiro numero");
+double SecondValue = Convert.ToDouble(Console.ReadLine());
+
+double result = 0;
+
+bool aux = true;
+do{
+    Console.WriteLine("Selecione a operação");
+    Console.WriteLine("1 - Adição");
+    Console.WriteLine("2 - Subtração");
+    Console.WriteLine("3 - Multiplicação");
+    Console.WriteLine("4 - Divisão");
+    Console.WriteLine("5 - Potênciação");
+    Console.WriteLine("0 - Sair");
+
+    int menu = 0;
+
+    try
+    {
+        menu = Convert.ToInt32(Console.ReadLine());
+
+        switch(menu)
+        {
+            case 1:
+                result  = FirstValue + SecondValue;
+                Console.WriteLine($"O resultado é igual a {result}");
+
+            break;
+            case 2:
+                result  = FirstValue - SecondValue;
+                Console.WriteLine($"O resultado é igual a {result}");
+            break;
+            case 3:
+                result  = FirstValue * SecondValue;
+                Console.WriteLine($"O resultado é igual a {result}");
+            break;
+            case 4:
+                result  = FirstValue / SecondValue;
+                if (SecondValue != 0) 
+                    	Console.WriteLine($"O resultado é igual a {result}");
+                else Console.WriteLine("Selecionar valor diferente de 0.");
+            break;
+            case 5:
+                result = Math.Pow(FirstValue, SecondValue);
+                Console.WriteLine($"O resultado é igual a {result}");
+            break;
+            case 0:
+
+                aux = false;
+            break;
+
+            default:
+                                        
+            break;
+        }
+    }
+    catch
+    {
+        Console.WriteLine("Opção inválida.");
+        menu = -1;
+        aux = true;
+    }
+
+}while(aux);
---
+++ Prog1_24/Atividades/Tabuada/Program.cs
@@ -0,0 +1,73 @@
+﻿// Lê o primeiro valor
+Console.WriteLine("Determine o valor do primeiro numero");
+double FirstValue = Convert.ToDouble(Console.ReadLine());
+
+
+double result = 0;
+
+bool aux = true;
+do{
+    Console.WriteLine("Selecione a operação");
+    Console.WriteLine("1 - Adição");
+    Console.WriteLine("2 - Subtração");
+    Console.WriteLine("3 - Multiplicação");
+    Console.WriteLine("4 - Divisão");
+    Console.WriteLine("0 - Sair");
+
+    int menu = 0;
+
+    try
+    {
+        menu = Convert.ToInt32(Console.ReadLine());
+
+        switch(menu)
+        {
+            case 1:
+                for (int i = 1; i <= 9; i++) 
+                {
+                    result = FirstValue + i;
+                    Console.WriteLine($"Tabuada de soma de {FirstValue} + {i} = {result}");
+                }
+
+            break;
+            case 2:
+                for (int i = 1; i <= 9; i++) 
+                {
+                    result = FirstValue - i;
+                    Console.WriteLine($"Tabuada de soma de {FirstValue} - {i} = {result}");
+                }
+            break;
+            case 3:
+                for (int i = 1; i <= 9; i++) 
+                {
+                    result = FirstValue * i;
+                    Console.WriteLine($"Tabuada de soma de {FirstValue} * {i} = {result}");
+                }
+            break;
+            case 4:
+                if (FirstValue != 0) 
+                    for (int i = 1; i <= 104; i++) 
+                    {
+                        result = FirstValue / i;
+                        Console.WriteLine($"Tabuada de soma de {FirstValue} / {i} = {result}");
+                    }
+                else Console.WriteLine("Selecionar valor diferente de 0.");
+            break;
+            case 0:
+
+                aux = false;
+            break;
+
+            default:
+                                        
+            break;
+        }
+    }
+    catch
+    {
+        Console.WriteLine("Opção inválida.");
+        menu = -1;
+        aux = true;
+    }
+
+}while(aux);
